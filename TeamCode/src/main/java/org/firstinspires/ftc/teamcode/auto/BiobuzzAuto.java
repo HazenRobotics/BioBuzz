05bbdf2cecdde6059781bcf975c6db9b238cf2ab
314ish <ruckusflashlight@gmail.com>
2026-09-21 20:55:27 -0700

    added auto class and changes to gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BiobuzzAuto.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+@Autonomous(name="mainAuto")
+public class BiobuzzAuto extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
