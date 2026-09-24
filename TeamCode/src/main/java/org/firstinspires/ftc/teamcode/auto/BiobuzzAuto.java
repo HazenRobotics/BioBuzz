3e8a705ab17db3607f49cdc53e5aa70afc0dca3c
314ish <ruckusflashlight@gmail.com>
2026-09-24 15:46:56 -0700

    imported ivy and created the poses for the south right scenario and a path to get to the shooting zone. Also added starter bot teleOp.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BiobuzzAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BiobuzzAuto.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import static com.pedropathing.api.Paths.line;
+
+import com.pedropathing.api.PoseFactory;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.pedropathing.paths.Path;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
@@ -7,8 +13,32 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 @Autonomous(name="mainAuto")
 public class BiobuzzAuto extends OpMode {
 
+    private Follower follower;
+    private final PoseFactory poseFactory = PoseFactory.degrees();
+
+
+    // create poses
+
+    // south red poses
+    private final Pose startSouthRed = poseFactory.of(60, 8.9, 90);
+    private final Pose shootSouthRed = poseFactory.of(60, 18.9, 90); // TODO: Make necessary adjustments once we know a good distance for shooting
+
+
+    // create path methods
+
+
+    // south red paths
+    private Path startSouthRedToScore() {
+        return line(startSouthRed, shootSouthRed).linear(startSouthRed, shootSouthRed);
+    }
+
     @Override
     public void init() {
+
+    }
+
+    @Override
+    public void start() {
 
     }
 
